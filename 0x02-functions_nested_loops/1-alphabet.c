bbe60782daec603c07fab9a01acfeef0c34f70c9
James Bawa <jbawa032@gmail.com>
2022-09-13 11:39:40 -0700

    script to print letters in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabets in lower case
+ *
+ * Return: Returns void.
+ */
+
+void print_alphabet(void)
+{
+	char lower = 'a';
+
+	while (lower <= 'z')
+		_putchar(lower);
+	lower++;
+
+	return (0);
+}
