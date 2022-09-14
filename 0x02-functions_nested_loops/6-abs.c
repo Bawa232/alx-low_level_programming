708264503dd4b174f448c65d7897650068e36647
James Bawa <jbawa032@gmail.com>
2022-09-13 17:47:50 -0700

    nice

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *_abs - function to return absolute value
+ * @r: An integre input
+ * Return: Absolut value of number r
+ */
+int _abs(int a)
+{
+	if (a >= 0)
+		return (a);
+	else
+		return (a * -1);
+}
