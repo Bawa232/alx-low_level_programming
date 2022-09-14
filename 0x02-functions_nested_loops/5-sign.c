670256b2d5a6eab635aca0ac142c8e4008c27371
James Bawa <jbawa032@gmail.com>
2022-09-13 17:29:48 -0700

    be good

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,16 +8,19 @@
 
 int print_sign(int n);
 {
-
+	int number;
 	if (n > 0)
 	{ _putchar('+');
-		return (1);
+		number = 1;
 	}
 	else if (n == 0)
 	{ _putchar('0');
-		return (0);
+		number = 0;
+	}
+	else
+	{ _putchar('-')
+		number = -1;
 	}
 
-
-	return (-1);
+	return (number);
 }
