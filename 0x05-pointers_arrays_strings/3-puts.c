d22d13d41be95c0bf3558becd2f125378c347f9b
James Bawa <jbawa032@gmail.com>
2022-09-19 06:21:22 -0700

    again

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -9,9 +9,8 @@ void _puts(char *str)
 {
 	int num = 0;
 
-	for (; num >= 0; num++)
-		if (str[num] != '\0')
-			_putchar(str[num]);
+	while (str[num] != '\0')
+		_putchar(str[num]);
 
 	_putchar('\n');
 }
