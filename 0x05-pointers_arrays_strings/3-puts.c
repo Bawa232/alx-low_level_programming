43d2d7dbd2d8a0080cd5ccf4de7feb7f1d839d3b
James Bawa <jbawa032@gmail.com>
2022-09-19 06:15:17 -0700

    hopeful

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints each character of a string
+ * @str: input string
+ * Returns: void
+ */
+void _puts(char *str)
+{
+	int num = 0;
+
+	for (; num >= 0; num++)
+		if (str[num] != '\0')
+			_putchar(str[num];
+
+	_putchar('\n');
+}
