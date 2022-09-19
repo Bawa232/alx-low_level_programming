76de248f6c4e10fc0e7045aa6f2be5997014efd7
James Bawa <jbawa032@gmail.com>
2022-09-19 05:26:53 -0700

    push

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - checks length of string
+ * @s: input character
+ * Return: Always 0
+ */
+
+int _strlen(char *s)
+{
+	int a = 0;
+
+	for (; s[a] != '\0'; a++)
+
+	return (a);
+}
